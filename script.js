988e151651156727d20e2b4bfbc5ceafe19b51c2
WarisRaja <warismehsi0463@gmail.com>
2025-01-02 11:40:12 +0530

    page1 and page2 half done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl=gsap.timeline();
+function loadingAnim(){
+  var tl=gsap.timeline();
 tl.from(".line h1",{
   y:150,
   stagger:0.25,
@@ -27,8 +28,9 @@ tl.to(".line h2", {
 tl.to("#loader",{
   opacity:0,
   duration:0.2,
-  delay:4,
+  delay:4,  
 });
+
 
 tl.from("#page1",{
   delay:0.2,
@@ -40,4 +42,34 @@ tl.from("#page1",{
 
 tl.to("#loader",{
   display:"none",
+});
+tl.from("#nav",{
+  opacity:0
 })
+tl.from("#hero1 h1 ,#hero2 h1,#hero3 h2,#hero4 h1",{
+  y:120,
+  stagger:0.2
+})
+};
+
+function cursorAnim(){
+  document.addEventListener("mousemove",function(details){
+    gsap.to("#crsr",{
+      left:details.x,
+      top:details.y
+    })
+  });
+  
+  // Shery.makeMagnet("#nav-part2 h4" , {
+    //  this is real
+  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  //   duration: 1,
+  // });
+  
+  
+  Shery.makeMagnet("#nav-part2 h4" );
+}
+
+loadingAnim();
+cursorAnim();
+
